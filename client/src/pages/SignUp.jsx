897e350a85cbe3cfe7be64bc5ff19bf7b8862364
HninThida <hninthida844@gmail.com>
2024-10-07 21:22:37 +0700

    create footer and fix viteconfig

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,43 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { postRequest } from "../utils/api";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+    console.log(formData);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      setErrorMessage("Add all required fields");
+    } else {
+      try {
+        setLoading(true);
+        setErrorMessage(null);
+        const res = await postRequest("auth/signup", formData);
+        console.log(res);
+        if (res.success === false) {
+          return setErrorMessage(res.message);
+        }
+        if (res.ok) {
+          navigate("/sign-in");
+        }
+        setLoading(false);
+      } catch (error) {
+        setErrorMessage(error.message);
+        setLoading(false);
+        console.log(error);
+      }
+    }
+  };
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl  flex-1 flex-col md:flex-row mx-auto md:items-center gap-5">
@@ -18,10 +54,15 @@ const SignUp = () => {
         </div>
 
         <div className="flex-1">
-          <form className="flex flex-col gap-5">
+          <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
             <div>
               <Label value="Your username" />
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Your email" />
@@ -29,14 +70,31 @@ const SignUp = () => {
                 type="email"
                 placeholder="name@company.com"
                 id="email"
+                onChange={handleChange}
               />
             </div>
             <div>
               <Label value="Your password" />
-              <TextInput type="password" placeholder="Password" id="password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit">
-              Sign Up
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm"></Spinner>
+                  <span>Loading ...</span>
+                </>
+              ) : (
+                "Sign Up"
+              )}
             </Button>
           </form>
           <div className="flex gap-2 mt-5 text-sm">
@@ -46,6 +104,11 @@ const SignUp = () => {
             </Link>
           </div>
         </div>
+        {errorMessage && (
+          <Alert className="mt-5" color="failure">
+            {errorMessage}
+          </Alert>
+        )}
       </div>
     </div>
   );
---
+++ client/src/utils/api.jsx
@@ -0,0 +1,44 @@
+export const userData = JSON.parse(localStorage.getItem("user"));
+
+const request = async (
+  route,
+  method,
+  body = null,
+  isPrivate = false,
+  headers = {}
+) => {
+  const options = {
+    method,
+    cache: "default",
+    headers: {
+      "Content-Type": "application/json",
+      ...headers,
+    },
+    ...(body && { body: JSON.stringify(body) }),
+  };
+
+  // Add Authorization header for private requests
+  if (isPrivate) {
+    const token = localStorage.getItem("token");
+    if (!token) throw new Error("No authorization token found!");
+    options.headers.Authorization = `Bearer ${token}`;
+  }
+  const response = await fetch(`/api/${route}`, options);
+  return await response.json();
+};
+
+// Generic HTTP methods that can handle both public and private requests
+export const getRequest = (route, isPrivate = false, headers = {}) =>
+  request(route, "GET", null, isPrivate, headers);
+
+export const postRequest = (route, body, isPrivate = false, headers = {}) =>
+  request(route, "POST", body, isPrivate, headers);
+
+export const putRequest = (route, body, isPrivate = false, headers = {}) =>
+  request(route, "PUT", body, isPrivate, headers);
+
+export const patchRequest = (route, body, isPrivate = false, headers = {}) =>
+  request(route, "PATCH", body, isPrivate, headers);
+
+export const deleteRequest = (route, isPrivate = false, headers = {}) =>
+  request(route, "DELETE", null, isPrivate, headers);
