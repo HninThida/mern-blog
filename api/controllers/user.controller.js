cd3a1108b5b17738f9dfe4e2337a9f5863a8ffc1
HninThida <hninthida844@gmail.com>
2024-10-07 17:17:41 +0700

    create a test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+const test = (req, res) => {
+  res.json({ message: "Api test" });
+};
+
+export default test;
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import exprees from "express";
 import mongoose from "mongoose";
+import userRoutes from "./routes/user-route.js";
 
 dotenv.config();
 
@@ -14,3 +15,5 @@ const app = exprees();
 app.listen(3000, () => {
   console.log("Server is running on 3000 !!");
 });
+
+app.use("/api/user", userRoutes);
